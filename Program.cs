c2c0834350edae6155921c1734e7a015006c024f
Thomas Hallström <minimoto@hotmail.com>
2025-01-27 21:17:41 +0100

    Adding Azure Key Vault

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+var keyVaultName = "PizzaLovervault";
+var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
+
+builder.Configuration.AddAzureKeyVault(
+    keyVaultUri,
+    new DefaultAzureCredential());
+
+var secretClient = new SecretClient(keyVaultUri, new DefaultAzureCredential());
+var databaseSecret = secretClient.GetSecret("sqlServer").Value;
+
+// Sätt anslutningssträngen i konfigurationen
+builder.Configuration["ConnectionStrings:umbracoDbDSN"] = databaseSecret.Value;
 
 builder.Services.AddCors(options =>
 {
